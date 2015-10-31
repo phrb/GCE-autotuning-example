989c46506bf03bfb5fe0b0d486291fe81b93f0de
phrb <pedro.bruel@gmail.com>
2015-10-31 20:31:20 -0200

    Add packages to startupscript.

--- run.py
+++ run.py
@@ -2,11 +2,11 @@
 
 from subprocess import call
 
-repo = "--repo=https://github.com/phrb/GCE-autotuning-example.git"
-project = "--project=just-clover-107416"
+repo           = "--repo=https://github.com/phrb/GCE-autotuning-example.git"
+project        = "--project=just-clover-107416"
 interface_path = "--interface-path=rosenbrock.py"
 interface_name = "--interface-name=Rosenbrock"
-parallelism = "--parallelism=4"
+parallelism    = "--parallelism=4"
 
 cmd = "python rosenbrock.py {0} {1} {2} {3} {4}".format(repo,
                                                         project,
