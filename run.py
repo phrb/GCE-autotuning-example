8dd10d2e73962c44f6bd4b5972aa5b8b6600787c
phrb <pedro.bruel@gmail.com>
2015-10-31 21:08:07 -0200

    Remove adddeps. Change parameters.

--- run.py
+++ run.py
@@ -6,13 +6,15 @@ repo           = "--repo=https://github.com/phrb/GCE-autotuning-example.git"
 project        = "--project=just-clover-107416"
 interface_path = "--interface-path=rosenbrock.py"
 interface_name = "--interface-name=Rosenbrock"
-parallelism    = "--parallelism=4"
+parallelism    = "--parallelism=8"
+results_log    = "--results-log=results.log"
 
-cmd = "python rosenbrock.py {0} {1} {2} {3} {4}".format(repo,
-                                                        project,
-                                                        interface_path,
-                                                        interface_name,
-                                                        parallelism)
+cmd = "python rosenbrock.py {0} {1} {2} {3} {4} {5}".format(repo,
+                                                            project,
+                                                            interface_path,
+                                                            interface_name,
+                                                            parallelism,
+                                                            results_log)
 
 print cmd
 
