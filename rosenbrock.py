b5b9ad69ec3c6c1c1cb042605951d2b4eed44bac
phrb <pedro.bruel@gmail.com>
2015-11-06 21:07:06 -0200

    Add .gitignore to submodule.

--- rosenbrock.py
+++ rosenbrock.py
@@ -4,10 +4,13 @@ import argparse
 import logging
 
 import opentuner
-from opentuner.measurement import MeasurementInterface, MeasurementDriver
+from opentuner.measurement import MeasurementInterface
 from opentuner.search.manipulator import ConfigurationManipulator
 from opentuner.search.manipulator import FloatParameter
-from GCEInterface.interface import GCEInterface
+
+import MeasurementClient
+from MeasurementClient.client import MeasurementClient
+from MeasurementClient.GCEInterface.interface import GCEInterface
 
 log = logging.getLogger(__name__)
 
@@ -19,84 +22,6 @@ parser.add_argument('--domain', type=float, default=1000,
 parser.add_argument('--function', default='rosenbrock',
                     choices=('rosenbrock', 'sphere', 'beale'),
                     help='function to use')
-parser.add_argument('--zone',
-                    type     = str,
-                    default  = "us-central1-f",
-                    help     = "The GCE zone.")
-parser.add_argument('--project',
-                    type     = str,
-                    required = True,
-                    help     = "The GCE project.")
-parser.add_argument('--repo',
-                    type     = str,
-                    required = True,
-                    help     = "The git repository with the tuner.")
-parser.add_argument('--interface-path',
-                    type     = str,
-                    required = True,
-                    help     = "The path of your tuner in the repository.")
-parser.add_argument('--interface-name',
-                    type     = str,
-                    required = True,
-                    help     = "The name of your measurement interface.")
-
-class MeasurementClient(MeasurementDriver):
-    def __init__(self,
-                 measurement_interface,
-                 input_manager,
-                 **kwargs):
-        super(MeasurementClient, self).__init__(measurement_interface,
-                                                input_manager,
-                                                **kwargs)
-
-        self.gce_interface = GCEInterface(zone            = self.args.zone,
-                                          project         = self.args.project,
-                                          repo            = self.args.repo,
-                                          interface_path  = self.args.interface_path,
-                                          interface_name  = self.args.interface_name,
-                                          instance_number = self.args.parallelism)
-
-        self.gce_interface.create_and_connect_all()
-
-    def process_all(self):
-        self.lap_timer()
-        q = self.query_pending_desired_results()
-        desired_results = []
-
-        for dr in q.all():
-          if self.claim_desired_result(dr):
-            desired_results.append(dr)
-
-        self.run_desired_results(desired_results)
-
-    def rosenbrock(self, x0, x1):
-        val = 100.0 * (x1 - x0 ** 2) ** 2 + (x0 - 1) ** 2
-        return val
-
-    def run_desired_results(self, desired_results):
-        requests = []
-
-        for desired_result in desired_results:
-            desired_result.limit = self.run_time_limit(desired_result)
-
-            input = self.input_manager.select_input(desired_result)
-            self.session.add(input)
-            self.session.flush()
-
-            log.debug('running desired result %s on input %s', desired_result.id,
-                    input.id)
-
-            self.input_manager.before_run(desired_result, input)
-
-            requests.append((desired_result.configuration,
-                           input,
-                           desired_result.limit))
-
-        results = self.gce_interface.compute_results(requests)
-
-        for result, d_result, request in zip(results, desired_results, requests):
-            input = request[1]
-            self.report_result(d_result, result, input)
 
 class Rosenbrock(MeasurementInterface):
     def run(self, desired_result, input, limit):
