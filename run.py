68597cb160dd51d7f689cde6901a785c52233f0d
phrb <pedro.bruel@gmail.com>
2015-11-07 00:05:07 -0200

    Add a new parameter to 'run'.

--- run.py
+++ run.py
@@ -7,17 +7,19 @@ repo           = "--repo=https://github.com/phrb/GCE-autotuning-example.git"
 project        = "--project=autotuning-1116"
 interface_path = "--interface-path=rosenbrock.py"
 interface_name = "--interface-name=Rosenbrock"
-parallelism    = "--parallelism=2"
+instances      = "--instances=8"
+parallelism    = "--parallelism=64"
 results_log    = "--results-log=results.log"
 stop_after     = "--stop-after=200"
 
-cmd = "python rosenbrock.py {0} {1} {2} {3} {4} {5} {6}".format(stop_after,
-                                                                repo,
-                                                                project,
-                                                                interface_path,
-                                                                interface_name,
-                                                                parallelism,
-                                                                results_log)
+cmd = "python rosenbrock.py {0} {1} {2} {3} {4} {5} {6} {7}".format(stop_after,
+                                                                    repo,
+                                                                    project,
+                                                                    interface_path,
+                                                                    interface_name,
+                                                                    instances,
+                                                                    parallelism,
+                                                                    results_log)
 
 print cmd
 
