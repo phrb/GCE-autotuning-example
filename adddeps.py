46156d6722e44331ac45afe501608da89b481be9
phrb <pedro.bruel@gmail.com>
2015-11-07 18:27:10 -0200

    Trying to fix 'execfile'.

--- adddeps.py
+++ adddeps.py
@@ -1,6 +1,7 @@
 import os
-path = "../opentuner/opentuner/utils/adddeps.py"
 
-if os.path.isfile(path):
-    target = os.path.join(os.path.dirname(__file__), path)
+path = "../opentuner/opentuner/utils/adddeps.py"
+target = os.path.join(os.path.dirname(__file__), path)
+
+if os.path.isfile(target):
     execfile(target, dict(__file__=target))
