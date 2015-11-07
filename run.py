4389e985675ac34437c59264f9ba0387932e27ab
phrb <pedro.bruel@gmail.com>
2015-11-07 21:09:09 -0200

    Changes to run script.

--- run.py
+++ run.py
@@ -6,10 +6,10 @@ repo           = "--repo=https://github.com/phrb/gce_autotuning_example.git"
 project        = "--project=autotuning-1116"
 interface_path = "--interface-path=rosenbrock.py"
 interface_name = "--interface-name=Rosenbrock"
-instances      = "--instances=2"
-parallelism    = "--parallelism=8"
+instances      = "--instances=16"
+parallelism    = "--parallelism=256"
 results_log    = "--results-log=results.log"
-stop_after     = "--stop-after=200"
+stop_after     = "--stop-after=600"
 
 cmd = "python rosenbrock.py {0} {1} {2} {3} {4} {5} {6} {7}".format(stop_after,
                                                                     repo,
